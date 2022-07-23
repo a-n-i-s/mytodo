9440f369c3e909f0934dd011db523881c0da7896
a-n-i-s <anisur.ra7man@gmail.com>
2022-07-23 10:57:53 +0600

    secret key fix

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21r!g^$+d_f&k9s5^m9tecwp+!h@xrtsjx6l=_uzp_f(p$u7rg'
+SECRET_KEY = os.environ.get('SECRET_KEY','change this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
